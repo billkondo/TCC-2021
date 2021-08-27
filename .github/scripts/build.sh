ce56fd0e0d8c95fd010c9b1780d84e9fe3a5a0d9
William Hideki Kondo <willkondo@gmail.com>
2021-08-26 22:24:15 -0300

    Configurando CI/CD

---
+++ .github/scripts/build.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+echo "TCC Build"
+
+echo "[1] Monografia Build"
+cd monografia
+make
+cd ..
+
+pwd
---
+++ .github/scripts/pre-deploy.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+echo "Pre-Deploy"
+
+cp ./monografia/tese.pdf ./monografia.pdf
